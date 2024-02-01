6360c7e7e1fdc94b01343580b8326ac09b2e4c6c
Katherine Li <math4fun2015@gmail.com>
2024-02-01 17:52:39 -0500

    space for logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from "react";
-import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
-import TaskRoundedIcon from '@mui/icons-material/TaskRounded';
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import TaskRoundedIcon from "@mui/icons-material/TaskRounded";
 import { Link } from "react-router-dom";
-import CalendarMonthRoundedIcon from '@mui/icons-material/CalendarMonthRounded';
-import SchoolRoundedIcon from '@mui/icons-material/SchoolRounded';
-import TimerRoundedIcon from '@mui/icons-material/TimerRounded';
-import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
+import CalendarMonthRoundedIcon from "@mui/icons-material/CalendarMonthRounded";
+import SchoolRoundedIcon from "@mui/icons-material/SchoolRounded";
+import TimerRoundedIcon from "@mui/icons-material/TimerRounded";
+import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
 
 const Navbar = (props) => {
   return (
@@ -13,14 +13,14 @@ const Navbar = (props) => {
       className={`flex-col h-screen w-60 space-y-1 mt-0 relative bg-${props.secondary}-100`}
     >
       <div className="h-24 items-center justify-center flex p-3">
-        {/* <img src="../app-logo"></img> */}
+        {/*image goes here! */}
       </div>
       <div>
         <Link
           to="/"
           className={`h-12 rounded-lg items-center flex mx-4 p-3 bg-hover-${props.secondary}-200`}
         >
-          <HomeRoundedIcon className="text-gray-800"/>
+          <HomeRoundedIcon className="text-gray-800" />
           <div className="px-4 text-gray-800">Home</div>
         </Link>
       </div>
@@ -29,7 +29,7 @@ const Navbar = (props) => {
           to="/tasks"
           className={`h-12 rounded-lg items-center flex mx-4 p-3 bg-hover-${props.secondary}-200`}
         >
-          <TaskRoundedIcon className="text-gray-800"/>
+          <TaskRoundedIcon className="text-gray-800" />
           <div className="px-4 text-gray-800">Tasks</div>
         </Link>
       </div>
@@ -38,7 +38,7 @@ const Navbar = (props) => {
           to="/calendar"
           className={`h-12 rounded-lg items-center flex mx-4 p-3 bg-hover-${props.secondary}-200`}
         >
-          <CalendarMonthRoundedIcon className="text-gray-800"/>
+          <CalendarMonthRoundedIcon className="text-gray-800" />
           <div className="px-4 text-gray-800">Calendar</div>
         </Link>
       </div>
@@ -47,7 +47,7 @@ const Navbar = (props) => {
           to="/classes"
           className={`h-12 rounded-lg items-center flex mx-4 p-3 bg-hover-${props.secondary}-200`}
         >
-          <SchoolRoundedIcon className="text-gray-800"/>
+          <SchoolRoundedIcon className="text-gray-800" />
           <div className="px-4 text-gray-800">Classes</div>
         </Link>
       </div>
@@ -56,7 +56,7 @@ const Navbar = (props) => {
           to="/pomodoro"
           className={`h-12 rounded-lg items-center flex mx-4 p-3 bg-hover-${props.secondary}-200`}
         >
-          <TimerRoundedIcon className="text-gray-800"/>
+          <TimerRoundedIcon className="text-gray-800" />
           <div className="px-4 text-gray-800">Pomodoro</div>
         </Link>
       </div>
@@ -65,7 +65,7 @@ const Navbar = (props) => {
           to="/settings"
           className={`h-12 rounded-lg items-center flex mx-5 p-3 bg-hover-${props.secondary}-200`}
         >
-          <SettingsRoundedIcon className="text-gray-800"/>
+          <SettingsRoundedIcon className="text-gray-800" />
           <div className="px-4 text-gray-800">Settings</div>
         </Link>
       </div>
